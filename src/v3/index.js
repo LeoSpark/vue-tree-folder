d82a28732b592d88848ea061bc72f9f59f05f844
LeoSpark <leo-mars@outlook.com>
2019-06-10 23:57:51 +0800

    new test code

--- src/v3/index.js
+++ src/v3/index.js
@@ -1,14 +1,41 @@
 /* global Vue */
+
+const throttle = (fn, delay = 30, mustRunDelay = 50) => {
+    let timer = null;
+    let t_start;
+    return function () {
+        const context = this,
+            args = arguments,
+            t_curr = +new Date();
+        clearTimeout(timer);
+        if (!t_start) {
+            t_start = t_curr;
+        }
+        if (t_curr - t_start >= mustRunDelay) {
+            fn.apply(context, args);
+            t_start = t_curr;
+        } else {
+            timer = setTimeout(function () {
+                fn.apply(context, args);
+            }, delay);
+        }
+    };
+};
+
 Vue.component('virtual-tree-list', {
     template: '#virtual-tree-list-template',
     props: {
         treeList: Array,
-        itemHeight: 60 // px
+        itemHeight: Number // px
     },
     data() {
         return {
             currentIndex: 0,
-            viewsItemLength: 20
+            scrollTop: 0,
+            wrapperHeight: 0,
+            wrapperPaddingTop: 0,
+            wrapperPaddingBottom: 400,
+            viewsItemLength: 10
         }
     },
     computed: {
@@ -20,15 +47,47 @@ Vue.component('virtual-tree-list', {
             const currentStartIndex = currentIndex > maxStartIndex ? maxStartIndex : currentIndex,
                 currentEndIndex = (currentIndex + listLength - 1) > maxEndIndex ? maxEndIndex : (currentIndex + listLength - 1);
             
-            return this.treeList.slice(currentStartIndex, currentEndIndex);
+            return this.treeList.slice(currentStartIndex, currentEndIndex).map(item => String(item).padStart(5, '0'))
+        },
+        paddingStyle() {
+            return {
+                paddingTop: this.wrapperPaddingTop + 'px',
+                paddingBottom: this.wrapperPaddingBottom + 'px'
+            }
         }
+    },
+    watch: {
+        scrollTop() {
+            this.setCurrentPadding();
+        }
+    },
+    methods: {
+        updateScrollTop: throttle(function() {
+            this.scrollTop = this.$el.scrollTop;
+        }),
+        setWrapperHeight() {
+            this.wrapperHeight = this.itemHeight * this.treeList.length;
+        },
+        setCurrentPadding() {
+            const itemsHeight = this.currentViewList.length * this.itemHeight;
+            const scrollTop = this.scrollTop;
+            this.wrapperPaddingTop = scrollTop;
+            this.wrapperPaddingBottom = this.wrapperHeight - scrollTop - itemsHeight;
+        }
+    },
+    created() {
+        this.setWrapperHeight();
+        this.setCurrentPadding();
     }
 });
 
 const app = window.app = new Vue({
     el: '#app',
     template: `
-        <virtual-tree-list :treeList="treeList"></virtual-tree-list>
+        <virtual-tree-list 
+            :itemHeight="60"
+            :treeList="treeList"
+            ></virtual-tree-list>
     `,
     data: {
         treeList: []
