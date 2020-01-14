e56658fe6c504c422e8896ae02481289334d53bb
Leo <leo-mars@outlook.com>
2020-01-14 21:43:53 +0800

    抽离工具函数到 util 文件夹

--- src/v3/components/virtual-view.vue
+++ src/v3/components/virtual-view.vue
@@ -16,30 +16,7 @@
 
 <script>
 import TreeItem from './tree-item';
-
-// 节流函数
-
-const throttle = (fn, delay = 20, mustRunDelay = 30) => {
-    let timer = null;
-    let t_start;
-    return function () {
-        const context = this,
-            args = arguments,
-            t_curr = +new Date();
-        clearTimeout(timer);
-        if (!t_start) {
-            t_start = t_curr;
-        }
-        if (t_curr - t_start >= mustRunDelay) {
-            fn.apply(context, args);
-            t_start = t_curr;
-        } else {
-            timer = setTimeout(function () {
-                fn.apply(context, args);
-            }, delay);
-        }
-    };
-};
+import { throttle } from '../utils/index';
 
 export default  {
     props: {
---
+++ src/v3/utils/index.js
@@ -0,0 +1,22 @@
+// 节流函数
+export const throttle = (fn, delay = 20, mustRunDelay = 30) => {
+    let timer = null;
+    let t_start;
+    return function () {
+        const context = this,
+            args = arguments,
+            t_curr = +new Date();
+        clearTimeout(timer);
+        if (!t_start) {
+            t_start = t_curr;
+        }
+        if (t_curr - t_start >= mustRunDelay) {
+            fn.apply(context, args);
+            t_start = t_curr;
+        } else {
+            timer = setTimeout(function () {
+                fn.apply(context, args);
+            }, delay);
+        }
+    };
+};
